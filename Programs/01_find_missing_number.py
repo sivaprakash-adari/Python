9ff65603f945e5abe9708f3f6ed21141d5344932
Siva Prakash Adari <adari.sivaprakash@gmail.com>
2025-01-19 01:32:49 +0000

    Python Basics

---
+++ Programs/01_find_missing_number.py
@@ -0,0 +1,14 @@
+#
+#  Given an array nums containing n distinct numbers in the range [0, n], 
+#  return the only number in the range that is missing from the array.
+#
+
+def find_missing_number(nums):
+     result = 0
+     
+     for i,n in enumerate(nums):
+          print(nums[i])    
+     return result
+
+nums = [1,2,3,5]
+find_missing_number(nums)
